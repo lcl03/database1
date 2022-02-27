6e222d6af07837dc190c2468bd42696121a7bb30
lcl03 <1945417981@qq.com>
2022-02-27 12:15:14 +0800

    commit scr file

---
+++ src/第二章.java
@@ -0,0 +1,22 @@
+package java大学实用练习;
+import java.util.*;
+
+public class 第二章 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       /*int[] a= {1,2,3,4,5,6,7,8,9};
+       a[1]=900;
+       System.out.println(a[1]);
+      int b=a.length;
+      System.out.println(b);*/
+		Scanner reader=new Scanner(System.in);
+		System.out.println("请输入您的id号码");
+		String id=reader.next();
+		System.out.println("输出id:"+id);
+		
+       
+	}
+
+}
+
